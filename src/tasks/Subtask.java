0a3f62030bd8ee4356606a9800310e6312e5078d
gennadiyniki <nikitkogs@gmail.com>
2025-07-07 19:13:27 +0300

    ФЗ6

--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -7,14 +7,16 @@ public class Subtask extends tasks.Task {
         super(name,description);
         this.epicId = epicId;
     }
-    public Subtask(int id, String name, String description, tasks.TaskStatus taskStatus, int epicId){
-        super (id, name, description, taskStatus); //+epicId + taskStatus
-        this.epicId = epicId;
 
+    public Subtask(int id, String name, String description, tasks.TaskStatus taskStatus, int epicId) {
+        super(id, name, description, taskStatus);
+        this.epicId = epicId;
     }
+
     public int getEpicId() {
         return epicId;
     }
+
     @Override
     public String toString() {
         return "Subtask{" +
