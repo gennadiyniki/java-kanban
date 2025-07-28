62b69b1ec8ead473b453b5cfe61c63061768a1ab
gennadiyniki <nikitkogs@gmail.com>
2025-07-28 13:20:27 +0300

    ФЗ7
    # Conflicts:
    #	src/manager/Managers.java
    #	test/manager/FileBackedTaskManagerTest.java
    #	test/manager/InMemoryTaskManagerTest.java

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -6,7 +6,7 @@ public class Managers {
 
 
     public static TaskManager getDefaultManager() { // Пробел перед { отсутствует, это нормально
-        return new FileBackedTaskManager(new File("src/main/resources/data.csv"));
+        return new FileBackedTaskManager(new File("/Users/gennadiyniki/IdeaProjects/java-kanban/src/resourses/data.csv"));
     }
 
     public static HistoryManager getDefaultHistory() { // Пробел перед { отсутствует, это нормально
