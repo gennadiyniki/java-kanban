bc9b0208f1750977d3092f78cf233fdac6109fde
gennadiyniki <nikitkogs@gmail.com>
2025-07-28 14:14:05 +0300

    ФЗ7
    # Conflicts:
    #	src/manager/Managers.java
    #	test/manager/FileBackedTaskManagerTest.java
    #	test/manager/InMemoryTaskManagerTest.java

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -4,10 +4,10 @@ import java.io.File;
 
 public class Managers {
 
-
     public static TaskManager getDefaultManager() {
-
-        return new FileBackedTaskManager(new File("/Users/gennadiyniki/IdeaProjects/java-kanban/src/resourses/data.csv"));
+        return new FileBackedTaskManager(
+                new File("/Users/gennadiyniki/IdeaProjects/java-kanban/src/resourses/data.csv")
+        );
     }
 
     public static HistoryManager getDefaultHistory() {
