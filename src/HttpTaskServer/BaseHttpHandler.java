657675d048a6376d07d56d0c66ab2fa8ca1aa840
gennadiyniki <nikitkogs@gmail.com>
2025-08-10 12:49:17 +0300

    Исправил указанные замечания. # Conflicts: #	src/manager/Managers.java #	test/manager/FileBackedTaskManagerTest.java #	test/manager/InMemoryTaskManagerTest.java

--- src/HttpTaskServer/BaseHttpHandler.java
+++ src/HttpTaskServer/BaseHttpHandler.java
@@ -1,7 +1,7 @@
 package HttpTaskServer;
 
-import Adapters.DurationAdapter;
-import Adapters.LocalDateTimeAdapter;
+import adapters.DurationAdapter;
+import adapters.LocalDateTimeAdapter;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpExchange;
